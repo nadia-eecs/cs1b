2ee5cd0d4c8eda517e928f5091b6be6b04426600
nadia ahmed <ahmedn@uci.edu>
2022-10-11 12:45:41 -0700

    solution

--- hw/hw02/src/matrix.cpp
+++ hw/hw02/src/matrix.cpp
@@ -19,7 +19,7 @@ void fileIO(string filename, double A[][n1], int m1, double B[][n2], int m2) {
 	}
 }
 
-void matrixAddition(const double& A[][n1], int m1, const double& B[][n2], int m2, const double& res[][n1]) {
+void matrixAddition(const double A[][n1], int m1, const double B[][n2], int m2, const double res[][n1]) {
   // matrix addition is defined if and only if A and B have the same size
   if (n1 != n2 && m1 != m2) return;
   for (int i = 0; i < m1; ++i) {
@@ -29,7 +29,7 @@ void matrixAddition(const double& A[][n1], int m1, const double& B[][n2], int m2
   }
 }
 
-void matrixSubtraction(double A[][n1], int m1, double B[][n2], int m2) {
+void matrixSubtraction(double A[][n1], int m1, double B[][n2], res[][n1], double m2) {
   // matrix subtraction is defined if and only if A and B have the same size
   if (n1 != n2 && m1 != m2) return;
   for (int i = 0; i < m1; ++i) {
@@ -52,5 +52,18 @@ void matrixMultiplication(double A[][n1], int m1, double B[][n2], int m2, double
   }
 }
 
+void matrixTranspose(double M[][n], int m, double res[][m]) {
+  if (m < 0) return;
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      res[j][i] = M[i][j];
+    }
+  }
+}
 
-double matrixDeterminant(double M[][n], int m);
+double matrixDeterminant3D(double M[][n], int m) {
+  // check if matrix is 3 x 3
+  if (m != n && m != 3) return 0;
+  return M[0][0]*(M[1][1]*M[2][2] - M[1][2]*M[2][1]) - M[0][1]*(M[1][0]*M[2][2] - M[1][2]*M[2][0]) + M[0][2]*(M[1][0]*M[2][1] - M[1][1]*M[2][0];
+}
+       
--- hw/hw02/src/matrix.h
+++ hw/hw02/src/matrix.h
@@ -4,14 +4,16 @@
 #include <string>
 using namespace std;
 
-void fileIO(string filename, double A[][n], int m, double B[][n2], int m2);
+void fileIO(string filename, double A[][n1], int m1, double B[][n2], int m2);
 
-void matrixAddition(double A[][n1], int m1, double B[][n1], int m2);
+void matrixAddition(const double A[][n1], int m1, const double B[][n2], int m2, const double res[][n1]);
 
-void matrixSubtraction(double A[][n1], int m1, double B[][n1], int m2);
+void matrixSubtraction(double A[][n1], int m1, double B[][n2], res[][n1], double m2);
 
-void matrixMultiplication(double A[][n1], int m1, double B[][n1], int m2);
+void matrixMultiplication(double A[][n1], int m1, double B[][n2], int m2, double res[][n2]);
 
-double matrixDeterminant(double M[][n], int m);
+void matrixTranspose(double M[][n], int m, double res[][m]); 
+
+double matrixDeterminant3D(double M[][n], int m);
 
 #endif
