41e2a03fdd9b1142f7d62d7f86edc975075e3ec8
nadia ahmed <ahmedn@uci.edu>
2023-02-02 15:08:40 -0800

    updating files and adding tests

---
+++ hw/hw02/tests/00_generateNum_test/generateNum_test.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+#include "gen_number.h"
+
+using namespace std;
+
+int currentPoints_generateNumTest = 0;
+int maxPoints = 10;
+
+TEST(hw02GenerateNumTest, generateNumTest) {
+  x = generateNum();
+  y = generateNum();
+  z = generateNum();
+  EXPECT_TRUE((x >= 10) && (x < 100) && (x != y) && (x != z));
+
+  if (!HasFailure()) {
+    currentPoints_generateNumTest += 10;
+  }
+  cout << "\nCurrent POINTS Generate Number Test: " << currentPoints_generateNumTest << "/" << maxPoints << endl;
+}
+}
