7276f61993c96c707d15d1f9f3cb4d83f09a57ca
nadia ahmed <ahmedn@uci.edu>
2023-02-07 14:36:24 -0800

    added tests for post-processing

---
+++ hw/hw02/tests/07_isPrime_test/isPrime_test.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+#include "post_process.h"
+
+using namespace std;
+
+int currentPoints_isPrimeTest = 0;
+int maxPoints = 10;
+
+TEST(hw02IsPrime, isPrimeTest) {
+  EXPECT_TRUE(isPrime(29)) << "Prime Number test is failing.";
+  if (!HasFailure()) {
+    currentPoints_isPrimeTest += 10;
+  }
+  cout << "\nCurrent POINTS for the Prime Number Test: " << currentPoints_isPrimeTest << "/" << maxPoints << endl;
+}
